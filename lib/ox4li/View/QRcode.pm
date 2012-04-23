27cbec5f0ae61a881b589b4e4e9c189d8d0f284f
Charlie Harvey <charlie@ox4.org>
2012-04-23 20:35:39 +0100

    - Added QR code view and test file for t. Not that there's any tests.

---
+++ lib/ox4li/View/QRcode.pm
@@ -0,0 +1,30 @@
+package ox4li::View::QRcode;
+
+use strict;
+use base 'Catalyst::View::GD::Barcode::QRcode';
+
+=head1 NAME
+
+ox4li::View::QRcode - Catalyst GD::Barcode::QRcode View
+
+=head1 SYNOPSIS
+
+See L<ox4li>
+
+=head1 DESCRIPTION
+
+Catalyst GD::Barcode::QRcode View.
+
+=head1 AUTHOR
+
+Charlie Harvey,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+
